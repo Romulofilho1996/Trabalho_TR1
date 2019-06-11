e060df7929d4fb426e0d5e50471bde70ded68c50
Romulo de Vasconcelos Feijao Filho <romulofilho@Romulos-MacBook-Pro.local>
2019-06-11 19:27:20 -0300

    Leitura de arquivo e criação de quadros

--- simulador.cpp
+++ simulador.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <fstream>
 /**
  * Integrantes:
  * 
@@ -12,7 +14,36 @@
  * 
 **/
 using namespace std;
+long int size = 0;
+int posicaoTotal = 0;
+char mem[1000];
+char quadro[8];
+
+void pegarDadosEnvio(){
+	int buffer;
+	FILE *file;
+	file = fopen("text.bin", "rb");
+	fread(mem, sizeof(mem), 1, file);
+	fseek(file, 0L, SEEK_END);
+	size = ftell(file);
+}
+
+void montarQuadro(){
+	int j = 0;
+	for(int i = posicaoTotal; i < (posicaoTotal+8); i++){
+		quadro[j] = mem[i];
+		j++;
+	}
+	j = 0; posicaoTotal += 9;
+	printf("%s\n", quadro);
+}
 
 int main(int argc, char const *argv[]) {
+	int i = 0;
+	pegarDadosEnvio();
+	while(size != 0){
+		montarQuadro();
+		size -= 9;
+	}
     return 0;
 }
