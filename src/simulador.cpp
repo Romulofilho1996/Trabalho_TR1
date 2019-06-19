4d180fd9c2e47ab812ad28f476510e1a28646085
Christian Luis <christianlmc1@gmail.com>
2019-06-19 17:30:07 -0300

    Adicionando matriculas no simulador.cpp

--- src/simulador.cpp
+++ src/simulador.cpp
@@ -4,7 +4,17 @@
 #include <fstream>
 #include <iterator>
 #include "../include/wraper.hpp"
-
+/**
+ * Integrantes:
+ * 
+ * Christian Luis - 15/0153538
+ * Thiago Araújo da Silva - 15/0149832
+ * Rômulo de Vasconcelos Feijão Filho - 14/0031260
+ * Matheus Feitosa de Castro - 15/01412974
+ * Guilherme Andreúce- 14/01419615
+ * Gabriel Guerra - 15/0126247
+ * 
+**/
 using namespace std;
 
 int main(int argc, char const *argv[])
