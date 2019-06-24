b14307181558225f8330c4144ed4e7818b885f3a
Matheus Castro <matheus.castro@brisabr.com.br>
2019-06-24 11:31:57 -0300

    bit paridade par e impar

--- src/enlace.cpp
+++ src/enlace.cpp
@@ -1,8 +1,10 @@
 #include "../include/enlace.hpp"
 
-enlace::enlace(int *quadro_inicial)
+enlace::enlace(int *quadro_inicial, int quadro_tamanho)
 {
-    setQuadro(quadro_inicial);
+    // setQuadro(quadro_inicial);
+    this->quadro = quadro_inicial;
+    this->quadro_tamanho = quadro_tamanho;
 }
 
 enlace::~enlace()
@@ -45,10 +47,10 @@ void enlace::CamadaEnlaceDadosTransmissoraControleDeErro()
     switch (tipoDeControleDeErro)
     {
     case 0: //bit de paridade par
-        //codigo
+        CamadaEnlaceDadosTransmissoraControleDeErroBitParidadePar();
         break;
     case 1: //bit de paridade impar
-        //codigo
+        CamadaEnlaceDadosTransmissoraControleDeErroBitParidadeImpar();
         break;
     case 2: //CRC
     //codigo
@@ -64,10 +66,52 @@ void enlace::CamadaEnlaceDadosTransmissoraControleDeFluxo()
 
 void enlace::CamadaEnlaceDadosTransmissoraControleDeErroBitParidadePar()
 {
+    int *quadro_codificado;
+    int novo_tamanho = this->quadro_tamanho + 1;
+    bool paridade = false;
+    quadro_codificado = (int *)malloc(sizeof(int) * novo_tamanho);
+    cout << "\n=========Transmissão Codificada Paridade Par=========" << endl;
+    for (int i = 0; i < this->quadro_tamanho; i++)
+    {
+        if(this->quadro[i] == 1) {
+            paridade = !paridade;
+        }
+        quadro_codificado[i] = this->quadro[i];
+    }
+    quadro_codificado[this->quadro_tamanho] = int(paridade);
+    for (int i = 0; i < novo_tamanho; i++)
+    {
+        cout << quadro_codificado[i];
+    }
+    cout << endl;
+    // free(this->quadro);
+    this->quadro = quadro_codificado;
+    this->quadro_tamanho = novo_tamanho;
 }
 
 void enlace::CamadaEnlaceDadosTransmissoraControleDeErroBitParidadeImpar()
 {
+    int *quadro_codificado;
+    int novo_tamanho = this->quadro_tamanho + 1;
+    bool paridade = true;
+    quadro_codificado = (int *)malloc(sizeof(int) * novo_tamanho);
+    cout << "\n=========Transmissão Codificada Paridade Par=========" << endl;
+    for (int i = 0; i < this->quadro_tamanho; i++)
+    {
+        if(this->quadro[i] == 1) {
+            paridade = !paridade;
+        }
+        quadro_codificado[i] = this->quadro[i];
+    }
+    quadro_codificado[this->quadro_tamanho] = int(paridade);
+    for (int i = 0; i < novo_tamanho; i++)
+    {
+        cout << quadro_codificado[i];
+    }
+    cout << endl;
+    // free(this->quadro);
+    this->quadro = quadro_codificado;
+    this->quadro_tamanho = novo_tamanho;
 }
 
 void enlace::CamadaEnlaceDadosTransmissoraControleDeErroCRC()
@@ -88,10 +132,10 @@ void enlace::CamadaEnlaceDadosReceptoraControleDeErro()
     switch (tipoDeControleDeErro)
     {
     case 0: //bit de paridade par
-        //codigo
+        CamadaEnlaceDadosReceptoraControleDeErroBitDeParidadePar();
         break;
     case 1: //bit de paridade impar
-        //codigo
+        CamadaEnlaceDadosReceptoraControleDeErroBitDeParidadeImpar();
         break;
     case 2: //CRC
     //codigo
@@ -107,10 +151,50 @@ void enlace::CamadaEnlaceDadosReceptoraControleDeFluxo()
 
 void enlace::CamadaEnlaceDadosReceptoraControleDeErroBitDeParidadePar()
 {
+    int *quadro_codificado = (int *)malloc(sizeof(int) * this->quadro_tamanho);
+    int novo_tamanho = this->quadro_tamanho - 1;
+    bool paridade = false;
+    cout << "\n=========Recepção Codificada Paridade Par=========" << endl;
+    for (int i = 0; i < novo_tamanho; i++)
+    {
+        if(this->quadro[i] == 1) {
+            paridade = !paridade;
+        }
+        quadro_codificado[i] = this->quadro[i];
+        cout << quadro_codificado[i];
+    }
+    cout << this->quadro[novo_tamanho];
+    if(this->quadro[novo_tamanho] != int(paridade)) {
+        cout << endl << "Erro na mensagem" << endl;
+    }
+    cout << endl;
+    free(this->quadro);
+    this->quadro = quadro_codificado;
+    this->quadro_tamanho = novo_tamanho;
 }
 
 void enlace::CamadaEnlaceDadosReceptoraControleDeErroBitDeParidadeImpar()
 {
+    int *quadro_codificado = (int *)malloc(sizeof(int) * this->quadro_tamanho);
+    int novo_tamanho = this->quadro_tamanho - 1;
+    bool paridade = true;
+    cout << "\n=========Recepção Codificada Paridade Par=========" << endl;
+    for (int i = 0; i < novo_tamanho; i++)
+    {
+        if(this->quadro[i] == 1) {
+            paridade = !paridade;
+        }
+        quadro_codificado[i] = this->quadro[i];
+        cout << quadro_codificado[i];
+    }
+    cout << this->quadro[novo_tamanho];
+    if(this->quadro[novo_tamanho] != int(paridade)) {
+        cout << endl << "Erro na mensagem" << endl;
+    }
+    cout << endl;
+    free(this->quadro);
+    this->quadro = quadro_codificado;
+    this->quadro_tamanho = novo_tamanho;
 }
 
 void enlace::CamadaEnlaceDadosReceptoraControleDeErroCRC()
