d9ad0b7a5ae9b0c7839368da202d7beee9619f56
Christian <christianlmc1@gmail.com>
2019-06-26 10:02:03 -0300

    Documentando CRC

--- include/enlace.hpp
+++ include/enlace.hpp
@@ -35,6 +35,20 @@ public:
      * a cada '1' encontrado, para então adicionar a variável ao quadro.
      */
     void CamadaEnlaceDadosTransmissoraControleDeErroBitParidadeImpar();
+
+    /**
+     * @brief Realiza o codificação CRC-32 para controle de erro.
+     * Primeiramente, é gerado um polinômio de grau 32. Em 
+     * seguida, a partir do quadro original, geramos um novo
+     * quadro com (32-1) bits acrescentados ao final do quadro
+     * original preenchidos com 0. Neste novo quadro, fazemos
+     * a operação de XOR com o polinomio gerado (CRC) sempre que
+     * for encontrado o bit 1. A operação de XOR é realizada até
+     * que se chegue a posição do ultimo bit do quadro original.
+     * Em seguida, é feita a soma bit a bit do quadro novo com
+     * o quadro CRC, e este novo quadro é então enviado
+     * 
+     */
     void CamadaEnlaceDadosTransmissoraControleDeErroCRC();
     void CamadaEnlaceDadosTransmissoraControleDeErroCodigoDeHamming();
 
@@ -61,6 +75,15 @@ public:
      * Caso sejam diferentes, houve erro na transmissão.
      */
     void CamadaEnlaceDadosReceptoraControleDeErroBitDeParidadeImpar();
+    /**
+     * @brief Realiza o codificação CRC-32 para controle de erro.
+     * Com o mesmo polinomio gerado na transmissão, é feito a 
+     * operação de XOR com o quadro recebido até a posição do
+     * (tamanho do quadro-(32-1)), se o resultado obtido dessas
+     * operações for 0, então o quadro foi recebido corretamente.
+     * Caso sejam diferentes, houve erro na transmissão.
+     * 
+     */
     void CamadaEnlaceDadosReceptoraControleDeErroCRC();
     void CamadaEnlaceDadosReceptoraControleDeErroCodigoDeHamming();
 
